91eccaae2ccb61fbeffc3c0eb1e296e22fa7f12e
Natha Lopes Zerbo <nathalopeszerbo7@gmail.com>
2024-05-18 15:50:03 -0300

    Estruturando Sidebar

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,10 +1,29 @@
+
+// Abrir menu Sidebar
 $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('.sidebar').addClass('active');
-        $('.menu-btn').css("visibility", 'visible');
+        $('.overlay').addClass("cor");
     });
     $('.close-btn').click(function(){
         $('.sidebar').removeClass('active');
-        $('.menu-btn').css("visibility", 'visible');
+        $('.overlay').removeClass("cor");
     })
 })
+
+
+// Dropdown dentro do Sidebar
+$(document).ready(function(){
+    $('.sub-btn').click(function(){
+    $(this).next('.sub-menu').slideToggle();
+    $(this).find('.dropdown').toggleClass('rotate');
+    });
+});
+
+$(document).ready(function(){
+    $('.menu-btn').click(function(){
+        $(this).find('')
+    })
+})
+
+
