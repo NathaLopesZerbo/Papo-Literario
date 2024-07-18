54e498cbce6799805d2054481d170b50a9de4f7f
Natha Lopes Zerbo <nathalopeszerbo7@gmail.com>
2024-07-18 01:56:43 -0300

    Adicionando o JS

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,17 +1,14 @@
-$(document).ready(function(){
-    $('.menu-btn').click(function(){
-        $('.sidebar').addClass('active');
-        $('.overlay').addClass("cor");
-        document.body.classList.add("no-scroll");
-    });
-    $('.close-btn').click(function(){
-        $('.sidebar').removeClass('active');
-        $('.overlay').removeClass("cor");
-        document.body.classList.remove("no-scroll");
-    })
-})
 
-$(document).ready(function(){
+ const modal = document.getElementById('modal')
+ const openModal = document.getElementById('openModal')
+ const closeModal = document.getElementById('closeModal')
+ const cardCompra = document.getElementById('card-compra')
+ const overlay = document.getElementById('overlay')
+ const openSidebar = document.getElementById('menu-btn')
+ const sidebar = document.getElementById('sidebar')
+ const closeSidebar = document.getElementById('close-btn')
+
+ $(document).ready(function(){
     $('.sub-btn').click(function(){
     $(this).next('.sub-menu').slideToggle();
     $(this).find('.dropdown').toggleClass('rotate');
@@ -19,11 +16,18 @@ $(document).ready(function(){
 });
 
 
- const modal = document.getElementById('modal')
- const openModal = document.getElementById('openModal')
- const closeModal = document.getElementById('closeModal')
- const cardCompra = document.getElementById('card-compra')
- const overlay = document.getElementById('overlay')
+ openSidebar.addEventListener('click',function(){
+    sidebar.classList.add('active')
+    overlay.style.display = "flex"
+    document.body.classList.add("no-scroll");
+ })
+
+ 
+ closeSidebar.addEventListener('click',function(){
+    sidebar.classList.remove('active')
+    overlay.style.display = "none"
+    document.body.classList.remove("no-scroll");
+})
 
  
 
