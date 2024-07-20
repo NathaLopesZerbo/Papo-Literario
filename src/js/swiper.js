3daa349007731c4a70315e453d4d6732880c81f2
Natha Lopes Zerbo <nathalopeszerbo7@gmail.com>
2024-07-20 18:42:06 -0300

    Adicionando carrossel ao card de livros

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,8 +1,6 @@
-const swiper = new Swiper('.slider-wrapper', {
+const swiperCards = new Swiper('.slider-wrapper', {
     loop: false,
-    grabCursor: true,
     spaceBetween: 10,
-
     navigation: {
       nextEl: '.button-prev',
       prevEl: '.button-next',
@@ -24,13 +22,35 @@ const swiper = new Swiper('.slider-wrapper', {
   
   const swiperCarrossel = new Swiper('.carrossel', {
     loop: true,
-
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
 
   });
+
+
+  const carrosseLivros = new Swiper('.mySwiper', {
+      navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+
+    breakpoints: {
+      0: {
+        slidesPerView:3,
+      },
+      768: {
+        slidesPerView:4
+      },
+      1024: {
+        slidesPerView:5
+      },
+   }
+
+  });
+
 
 
 
