86a9bb90bdce9c150c5e833908dff73774b2aaa6
Natha Lopes Zerbo <nathalopeszerbo7@gmail.com>
2024-07-24 16:31:22 -0300

    Atualizando chat

---
+++ chat-realtime-yt/socket/index.js
@@ -0,0 +1,39 @@
+const { Server } = require('socket.io');
+const io = new Server({ cors: { origin: 'http://localhost:5173', credentials: true } });
+
+let onlineUsers = [];
+
+io.on('connection', (socket) => {
+  console.log('new connection', socket.id);
+
+  // conexão personalizada para ter o id do usuário
+
+  socket.on('addNewUser', (userId) =>{
+    !onlineUsers.some(user => user.userId === userId) &&
+      onlineUsers.push({
+        userId,
+        socketId: socket.id,
+      });
+      console.log('onlineUser', onlineUsers);
+
+      io.emit('getOnlineUsers', onlineUsers);
+  });
+
+  // enviar mensagem 
+
+  socket.on('SendMessage', (message) => {
+    const user = onlineUsers.find(user => user.userId === message.recipientId)
+
+    if(user){
+      io.to(user.socketId).emit('getMessage', message);
+    }
+  })
+
+  // desconectar o usuário
+  socket.on('disconnect', () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+
+  io.emit('getOnlineUsers', onlineUsers);
+  })
+});
+io.listen(3000);
