65c9be183fc159d678c035472ced6428aa7a6acd
Nathã Lopes Zerbo <nathalopeszerbo7@gmail.com>
2024-07-25 08:42:55 -0300

    Atualizando

--- src/js/compra.js
+++ src/js/compra.js
@@ -6,6 +6,8 @@
  const openSidebar = document.getElementById('menu-btn')
  const sidebar = document.getElementById('sidebar')
  const closeSidebar = document.getElementById('close-btn')
+ const dropdown = document.getElementById('dropdown')
+ const dropdownContent = document.getElementById('dropdownContent')
 
  $(document).ready(function(){
     $('.sub-btn').click(function(){
@@ -14,6 +16,22 @@
     });
 });
 
+
+let timeout;
+
+function showDropdown() {
+    clearTimeout(timeout);
+    dropdownContent.style.display = 'block';
+}
+
+dropdown.addEventListener('mouseover', showDropdown);
+dropdown.addEventListener('mouseout', hideDropdown);
+
+function hideDropdown() {
+    timeout = setTimeout(() => {
+        dropdownContent.style.display = 'none';
+    }, 100);
+}
 
 openSidebar.addEventListener('click',function(){
     sidebar.classList.add('active')
