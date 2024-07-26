90af354c4e2527f0a83575f1b1030681e5554fb4
Natha Lopes Zerbo <nathalopeszerbo7@gmail.com>
2024-07-25 22:11:06 -0300

    Atualizando

--- chat-realtime-yt/server/routes/messageRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMessages, createMessage } = require('../controllers/messageController');
-
-router.get('/:chatId', getMessages);
-router.post('/', createMessage);
-
-module.exports = router;
