4adf5781edb52267a5b954f4cf122b968e352778
Nathã Lopes Zerbo <104437494+NathaLopesZerbo@users.noreply.github.com>
2025-02-23 20:49:29 -0300

    Delete reddit-projeto directory
    Signed-off-by: Nathã Lopes Zerbo <104437494+NathaLopesZerbo@users.noreply.github.com>

--- reddit-projeto/client/src/Input.js
+++
@@ -1,7 +0,0 @@
-function Input(props) {
-    return (
-        <input {...props} className={'bg-gray-300 text-black p-2 border border-reddit_ligh rounded-md block '+props.className} />
-    );
-}
-
-export default Input;
--- reddit-projeto/client/src/Textarea.js
+++
@@ -1,7 +0,0 @@
-function Textarea(props) {
-    return (
-        <textarea {...props} className={'bg-gray-300 text-black p-2 border border-reddit_ligh rounded-md block '+props.className} />
-    );
-}
-
-export default Textarea;
